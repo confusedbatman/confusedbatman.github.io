afc7254942d0551c4ef5c86b528d57d5755d0f4e
confusedbatman <114649163+confusedbatman@users.noreply.github.com>
2022-10-01 23:24:45 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-alert("Hello JavaScript");
   let targetImage = document.querySelector("#smart-image");
   targetImage.addEventListener("click", function() {
-  targetImage.classList.remove("small");
     if (targetImage.classList.contains("small")) {
 targetImage.classList.remove("small");
 } else {
