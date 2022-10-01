135e445b10f7f24dae63c1467af62b70e60aecd2
confusedbatman <114649163+confusedbatman@users.noreply.github.com>
2022-10-01 23:19:13 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,5 +3,10 @@ alert("Hello JavaScript");
   let targetImage = document.querySelector("#smart-image");
   targetImage.addEventListener("click", function() {
   targetImage.classList.add("small");
+    if (targetImage.classList.contains("small")) {
+targetImage.classList.add("small");
+} else {
+targetImage.classList.remove("small");
+}
 });
 });
