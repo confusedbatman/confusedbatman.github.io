6265f8fbad44e427105f2afcff76dc1b2ceab904
confusedbatman <114649163+confusedbatman@users.noreply.github.com>
2022-10-01 22:19:08 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 alert("Hello JavaScript");
+  <img="https://www.annspangler.com/wp-content/uploads/2018/12/mountains-800px.jpg">
 });
